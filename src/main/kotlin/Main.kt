f1cfa82c72aa5b69572783c6ce6b527dc5f5053a
jolyyyyyyyyyyyy <amaliamironenko2804@mail.ru>
2022-04-10 17:29:48 +0300

    выполнила задание

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,53 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+    interface Publication {
+        abstract val notnull: Any
+        abstract var n: Any
+        val price: Int
+        val wordCount: Int
+
+        fun wordCount1() {
+            var wordCount1: Int
+        }
+
+        fun getType(text: String): String? {
+            return null
+        }
+        class Book(name: String){
+
+                fun Book(wordCount: Int): Unit {
+                    if (wordCount < 1000)
+                        println("Flash Fiction")
+                    else if (wordCount < 7500) {
+                        println("Short Story")
+            }
+                    else {
+                        println("Novel")
+                    }
+                }
+        }
+        class Magazine(){
+            fun Magazine(): Unit {
+                println("Magazine")
+            }
+        }
+
+        fun buy(){
+            var Publication: String = "n"
+            n = notnull.toString()
+            println("The purchase is complete. The purchase amount was $price")
+        }
+
+        fun Book() {
+            var a: String? = null
+            var b: Int? = 25
+            a?.let {
+                print(a.length)
+            }
+            b?.let {
+                print (buy())
+            }
+        }
+    }
+
